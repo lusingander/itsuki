dfcafffc77327da87beaf7782582c17ebbbcee26
Kyosuke Fujimoto <kyoro.f@gmail.com>
2024-05-11 14:24:05 +0900

    Fix test

--- tests/enum.rs
+++ tests/enum.rs
@@ -5,46 +5,7 @@ fn test_define_zero_indexed_enum() {
     define_zero_indexed_enum! {
         Quintuplets => [Ichika, Nino, Miku, Yotsuba, Itsuki]
     }
-
-    let miku = Quintuplets::Miku;
-
-    assert_eq!(miku, Quintuplets::Miku);
-    assert_ne!(miku, Quintuplets::Nino);
-
-    assert_eq!(Quintuplets::len(), 5);
-
-    use Quintuplets::*;
-
-    assert_eq!(
-        Quintuplets::vars_vec(),
-        vec![Ichika, Nino, Miku, Yotsuba, Itsuki]
-    );
-    assert_eq!(
-        Quintuplets::vars_array(),
-        [Ichika, Nino, Miku, Yotsuba, Itsuki]
-    );
-
-    assert_eq!(Nino.next(), Miku);
-    assert_eq!(Itsuki.next(), Ichika);
-
-    assert_eq!(Yotsuba.prev(), Miku);
-    assert_eq!(Ichika.prev(), Itsuki);
-
-    assert_eq!(Ichika.next_in(|q| [Miku, Yotsuba].contains(&q)), Miku);
-    assert_eq!(Miku.next_in(|q| [Miku, Yotsuba].contains(&q)), Yotsuba);
-
-    assert_eq!(Nino.prev_in(|q| [Miku, Yotsuba].contains(&q)), Yotsuba);
-    assert_eq!(Yotsuba.prev_in(|q| [Miku, Yotsuba].contains(&q)), Miku);
-
-    assert_eq!(Miku.val(), 2);
-    assert_eq!(Yotsuba.val(), 3);
-
-    assert_eq!(Quintuplets::try_from(0), Ok(Ichika));
-    assert_eq!(Quintuplets::try_from(4), Ok(Itsuki));
-    assert_eq!(Quintuplets::try_from(5), Err(()));
-
-    assert_eq!(1.try_into(), Ok(Nino));
-    assert_eq!(3.try_into(), Ok(Yotsuba));
+    test_quintuplets!();
 }
 
 #[test]
@@ -57,46 +18,7 @@ fn test_zero_indexed_enum_derive() {
         Yotsuba,
         Itsuki,
     }
-
-    let miku = Quintuplets::Miku;
-
-    assert_eq!(miku, Quintuplets::Miku);
-    assert_ne!(miku, Quintuplets::Nino);
-
-    assert_eq!(Quintuplets::len(), 5);
-
-    use Quintuplets::*;
-
-    assert_eq!(
-        Quintuplets::vars_vec(),
-        vec![Ichika, Nino, Miku, Yotsuba, Itsuki]
-    );
-    assert_eq!(
-        Quintuplets::vars_array(),
-        [Ichika, Nino, Miku, Yotsuba, Itsuki]
-    );
-
-    assert_eq!(Nino.next(), Miku);
-    assert_eq!(Itsuki.next(), Ichika);
-
-    assert_eq!(Yotsuba.prev(), Miku);
-    assert_eq!(Ichika.prev(), Itsuki);
-
-    assert_eq!(Ichika.next_in(|q| [Miku, Yotsuba].contains(&q)), Miku);
-    assert_eq!(Miku.next_in(|q| [Miku, Yotsuba].contains(&q)), Yotsuba);
-
-    assert_eq!(Nino.prev_in(|q| [Miku, Yotsuba].contains(&q)), Yotsuba);
-    assert_eq!(Yotsuba.prev_in(|q| [Miku, Yotsuba].contains(&q)), Miku);
-
-    assert_eq!(Miku.val(), 2);
-    assert_eq!(Yotsuba.val(), 3);
-
-    assert_eq!(Quintuplets::try_from(0), Ok(Ichika));
-    assert_eq!(Quintuplets::try_from(4), Ok(Itsuki));
-    assert_eq!(Quintuplets::try_from(5), Err(()));
-
-    assert_eq!(1.try_into(), Ok(Nino));
-    assert_eq!(3.try_into(), Ok(Yotsuba));
+    test_quintuplets!();
 }
 
 #[test]
@@ -109,44 +31,50 @@ fn test_zero_indexed_enum() {
         Yotsuba,
         Itsuki,
     }
+    test_quintuplets!();
+}
 
-    let miku = Quintuplets::Miku;
+#[macro_export]
+macro_rules! test_quintuplets {
+    () => {
+        let miku = Quintuplets::Miku;
 
-    assert_eq!(miku, Quintuplets::Miku);
-    assert_ne!(miku, Quintuplets::Nino);
+        assert_eq!(miku, Quintuplets::Miku);
+        assert_ne!(miku, Quintuplets::Nino);
 
-    assert_eq!(Quintuplets::len(), 5);
+        assert_eq!(Quintuplets::len(), 5);
 
-    use Quintuplets::*;
+        use Quintuplets::*;
 
-    assert_eq!(
-        Quintuplets::vars_vec(),
-        vec![Ichika, Nino, Miku, Yotsuba, Itsuki]
-    );
-    assert_eq!(
-        Quintuplets::vars_array(),
-        [Ichika, Nino, Miku, Yotsuba, Itsuki]
-    );
+        assert_eq!(
+            Quintuplets::vars_vec(),
+            vec![Ichika, Nino, Miku, Yotsuba, Itsuki]
+        );
+        assert_eq!(
+            Quintuplets::vars_array(),
+            [Ichika, Nino, Miku, Yotsuba, Itsuki]
+        );
 
-    assert_eq!(Nino.next(), Miku);
-    assert_eq!(Itsuki.next(), Ichika);
+        assert_eq!(Nino.next(), Miku);
+        assert_eq!(Itsuki.next(), Ichika);
 
-    assert_eq!(Yotsuba.prev(), Miku);
-    assert_eq!(Ichika.prev(), Itsuki);
+        assert_eq!(Yotsuba.prev(), Miku);
+        assert_eq!(Ichika.prev(), Itsuki);
 
-    assert_eq!(Ichika.next_in(|q| [Miku, Yotsuba].contains(&q)), Miku);
-    assert_eq!(Miku.next_in(|q| [Miku, Yotsuba].contains(&q)), Yotsuba);
+        assert_eq!(Ichika.next_in(|q| [Miku, Yotsuba].contains(&q)), Miku);
+        assert_eq!(Miku.next_in(|q| [Miku, Yotsuba].contains(&q)), Yotsuba);
 
-    assert_eq!(Nino.prev_in(|q| [Miku, Yotsuba].contains(&q)), Yotsuba);
-    assert_eq!(Yotsuba.prev_in(|q| [Miku, Yotsuba].contains(&q)), Miku);
+        assert_eq!(Nino.prev_in(|q| [Miku, Yotsuba].contains(&q)), Yotsuba);
+        assert_eq!(Yotsuba.prev_in(|q| [Miku, Yotsuba].contains(&q)), Miku);
 
-    assert_eq!(Miku.val(), 2);
-    assert_eq!(Yotsuba.val(), 3);
+        assert_eq!(Miku.val(), 2);
+        assert_eq!(Yotsuba.val(), 3);
 
-    assert_eq!(Quintuplets::try_from(0), Ok(Ichika));
-    assert_eq!(Quintuplets::try_from(4), Ok(Itsuki));
-    assert_eq!(Quintuplets::try_from(5), Err(()));
+        assert_eq!(Quintuplets::try_from(0), Ok(Ichika));
+        assert_eq!(Quintuplets::try_from(4), Ok(Itsuki));
+        assert_eq!(Quintuplets::try_from(5), Err(()));
 
-    assert_eq!(1.try_into(), Ok(Nino));
-    assert_eq!(3.try_into(), Ok(Yotsuba));
+        assert_eq!(1.try_into(), Ok(Nino));
+        assert_eq!(3.try_into(), Ok(Yotsuba));
+    };
 }
